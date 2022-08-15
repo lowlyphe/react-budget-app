75a4553e7df2c37d957acd099c342af8af8aeca8
lowlyphe <jones.kyle2893@gmail.com>
2022-08-15 16:57:49 -0400

    working on some styling

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,25 +1,30 @@
-import { currencyFormatter } from "../utils"
+import { currencyFormatter, getProgressBarVarient } from "../utils"
 
 
-export default function BudgetCard({ name, amount, max }) {
+export default function BudgetCard({ name, amount, max }) {    
     
     const progressStyle = {
-        width: `${(amount / max) * 100}%`
+        width: `${(amount / max) * 100}%`,
+        color: getProgressBarVarient(amount, max)
     }
+
+
   return (
     <div className="flex flex-col mx-4 my-4 border border-solid border-gray-500 rounded-md p-2 space-y-4">
         <div className="flex justify-between align-baseline ">
             <div className="flex items-baseline">{name}</div>
             <div>{currencyFormatter.format(amount)} / <span className="text-gray-500 text-xs">{currencyFormatter.format(max)}</span></div>
         </div>
-        <div class="w-full bg-gray-200 rounded-full h-3.5">
-            <div class="bg-blue-600 h-3.5 rounded-full" style={progressStyle}></div>
+        <div className="w-full bg-gray-200 rounded-full h-3.5">
+            <div className="h-3.5 rounded-full" style={progressStyle}></div>
         </div>
     </div>
   )
 
 
 }
+
+
 
 
 
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,11 @@ export const currencyFormatter = new Intl.NumberFormat(undefined, {
     style: "currency",
     minimumFractionDigits: 0
 });
+
+export const getProgressBarVarient = (amount, max) => {
+    const ratio = amount / max;
+    if (ratio < 0.5) return "blue";
+    if (ratio < 0.75) return "yellow";
+    return "red";
+
+}
